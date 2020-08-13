51a94d160705c7a9660e26e22ba637fe3b259fdd
Bharat Mane <bharat.mane@1985.co.in>
2020-08-13 14:31:10 +0530

    Popup Modal add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
-import PopUp from '../PopUp/PopUp'
+import PopupModal from '../PopUp/PopupModal'
 
 export class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            show: false
+        }
+        this.popupHandler = this.popupHandler.bind(this)
+        this.closeModal = this.closeModal.bind(this)
+    }
+    popupHandler(){
+        this.setState(state => ({
+            show: !this.state.show
+            })
+        )
+    }
+    closeModal(){
+        this.setState(state=>({
+            show: false
+        })
+        )
+    }
     render() {
         return (
             <div>
                 Home
-                <PopUp />
+                DropDown upcoming
+
+                <br/>
+                <button onClick={this.popupHandler} >Click for PopUp</button>
+                <PopupModal show={this.state.show} closeModal={this.closeModal} />
+                <br/>
             </div>
         )
     }
